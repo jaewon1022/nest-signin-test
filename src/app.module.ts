3f249bcad73c4d150713eb8202cfec6dd8e6e0fa
jaewon1022 <andyjaewon@naver.com>
2023-01-10 13:21:52 +0900

    auth 관련 컨트롤러, 서비스, 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
