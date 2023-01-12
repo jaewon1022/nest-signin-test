91a9260edf836e7a0524b27a84cee135644a0a7c
jaewon1022 <andyjaewon@naver.com>
2023-01-12 18:13:01 +0900

    함수형으로 리팩토링

--- src/auth/guard/register.guard.ts
+++ src/auth/guard/register.guard.ts
@@ -13,23 +13,22 @@ export class RegisterGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    return this.validateRequest(request);
+  }
+
+  private async validateRequest(request) {
+    const { email } = request.body;
     const prisma = new PrismaService();
     const authService = new AuthService(prisma);
 
-    const request = context.switchToHttp().getRequest();
+    const isExist = await authService.validateUserExist(email);
 
-    const { email } = request.body;
+    if (isExist) {
+      throw new ConflictException('이미 존재하는 이메일입니다.');
+    }
 
-    const getDatas = async () => {
-      const isExist = await authService.validateUserExist(email);
-
-      if (isExist) {
-        throw new ConflictException('이미 존재하는 이메일입니다.');
-      }
-
-      return true;
-    };
-
-    return getDatas();
+    return request;
   }
 }
